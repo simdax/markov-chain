a10d6d1637e1bf53de38c73cd2af6fb6452b75c1
simdax <simoncornaz@gmail.com>
2017-06-24 23:04:17 +0200

    set update

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ require.config({
     paths:{
        d3: "https://d3js.org/d3.v4.min",
        jquery:"https://code.jquery.com/jquery-3.1.0.min",
-       markov:'./markov',
-       svg:'./markovSvg'
+       // dunno any cdn for vue and require ???
+       // ==> dont work
+       // vue:"//unpkg.com/vue",
+       markov:'./lib/markov',
+       svg:'./lib/markovSvg'
     }
 })
 
@@ -15,27 +18,37 @@ require(['markov','svg'], function(MarkovChain,Draw) {
             {x: 200, y: 100},
         ],
         edges: [
-            {source: 0, target: 0, probability: 0.5},
             {source: 0, target: 1, probability: 0.5},
             {source: 1, target: 0, probability: 0.9},
-            {source: 1, target: 1, probability: 0.1}
+            {source: 1, target: 1, probability: 0.1},
+            {source: 0, target: 0, probability: 0.5},
         ]
     }
 
-
     var svg = new Draw(data);
     svg.draw();
-           markov = new MarkovChain(
-                [[0.5, 0.5],
-                 [0.9, 0.1]]
-            );
-    // window.add = _ =>{
-    //     var x = Math.random() * 300;
-    //     var y = Math.random() * 100;
-    //     data.nodes.push({x,y});
-    //     drawEdges(data);
-    // }
 
+    window.add = _ =>{
+        var x = Math.random() * 300;
+        var y = Math.random() * 100;
+        data.nodes.push({x,y});
+        data.edges.push({source: 1, target: 2, probability: 0.5});
+        svg.setEdges();
+        // svg = new Draw(data);
+    }
 
-    // simulate();
+    new Vue({
+      el:'#app',
+      data:data,
+      watch:{
+        edges(){
+          svg.draw()
+        }  
+      }
+    })
+
+    markov = new MarkovChain(
+        [[0.5, 0.5],
+         [0.9, 0.1]]
+    );
 });
