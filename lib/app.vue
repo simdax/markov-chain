2cd121c61b6864d46eb3ce4e56cf4d8ad9367b78
simdax <simoncornaz@gmail.com>
2017-06-27 22:47:14 +0200

    def architecture !

---
+++ lib/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div> 
+    <graph></graph> 
+    <settings></settings>
+  </div> 
+</template>
+
+<script>
+
+  import settings from './components/settings.vue'
+  import graph from "./components/svg.vue"
+
+  export default{
+    name: 'markov',
+    components:{settings,graph},
+  }
+
+</script>
+
+<style src="./style.css"></style>
