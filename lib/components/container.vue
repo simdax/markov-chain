409d56218383941511ebf41d2b57c55331e648f1
simdax <simoncornaz@gmail.com>
2017-06-28 18:17:52 +0200

    $1

---
+++ lib/components/container.vue
@@ -0,0 +1,37 @@
+<template>
+	
+	<div id="markovContainer">
+	  <button @click="create">fdsfsd</button>
+	  <markov v-for="k in nb" :key="k" :ns='k'></markov>
+	</div>		
+
+</template>
+
+<script>
+
+	import markov from './markov.vue'
+	// import * as main from '../main' 
+	var st = require("../main")
+
+	export default{
+		components: {markov},
+		data(){return{
+					nb:0
+				}},
+		methods:{
+			create(){
+				this.nb++;
+				this.$store.registerModule(''+this.nb,st.store);
+			}
+		}
+	}
+</script>
+
+<style>
+	#markovContainer{
+		display: flex;
+		flex-flow: row wrap;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
---
+++ lib/components/markov.vue
@@ -0,0 +1,28 @@
+<template>
+  <div> 
+    <graph :ns="ns" ref="graph"></graph> 
+    <div>
+      <edges :ns="ns" @opacity="update"></edges>
+      <nodes :ns="ns"></nodes>      
+    </div>  
+  </div> 
+</template>
+
+<script>
+
+  import edges from './edges.vue'
+  import nodes from './nodes.vue'
+  import graph from "./svg.vue"
+
+  export default{
+    props:['ns'],
+    name: 'markov',
+    components:{edges,nodes,graph},
+    methods:{
+      update(){this.$refs.graph.update()}
+    }
+  }
+
+</script>
+
+<style src="./style.css"></style>
